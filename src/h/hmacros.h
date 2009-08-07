e3a09e6751255f9005dc7f9143c8eb6de85b1573
Silvan Scherrer <silvan.scherrer@aroa.ch>
2009-08-07 11:43:42 +0000

    kmk makefile and related changes
    git-svn-id: http://svn.netlabs.org/repos/unipdr/branches/1.0@3 7e73b2ca-0781-de11-bd16-00e081727c95

---
+++ src/h/hmacros.h
@@ -0,0 +1,14 @@
+#define HEADING(a,b) :h##a res=##b group=2 x=left y=top width=100% \
+                     height=100% scroll=both.
+#define LINK(a,b) :link reftype=hd res=a.b:elink.
+#define PINDEX(a) :i1 id=a.
+#define SINDEX(a) :i2 refid=a.
+#define HP1(a) :hp1.##a##:ehp1.
+#define HP2(a) :hp2.##a##:ehp2.
+#define HP3(a) :hp3.##a##:ehp3.
+#define HP4(a) :hp4.##a##:ehp4.
+#define HP5(a) :hp5.##a##:ehp5.
+#define HP6(a) :hp6.##a##:ehp6.
+#define HP7(a) :hp7.##a##:ehp7.
+#define HP8(a) :hp8.##a##:ehp8.
+#define HP9(a) :hp9.##a##:ehp9.
